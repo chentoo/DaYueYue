0b073691069556eecbf34267f904055257e47b5e
chentoo <chentoo@gmail.com>
2014-05-30 13:34:56 +0800

    Initial Commit

---
+++ DaYueYue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DaYueYue
+//
+//  Created by xiazhidi on 14-5-30.
+//  Copyright (c) 2014年 chentoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DaYueYue/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DaYueYue
+//
+//  Created by xiazhidi on 14-5-30.
+//  Copyright (c) 2014年 chentoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
